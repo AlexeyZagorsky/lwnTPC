f58e138a9abf44376c8279deb90ed2d954d177ba
Alex Zagorsky <alexey.zagorsky@gmail.com>
2017-06-16 17:10:51 +0300

    Basic classes of lwnTPC. 1 todo item left. Currently we have the working prototype of lwnTPC. It can do the  initial normalization (setting the proper doctype and the meta charset tag) and file encoding conversion.

---
+++ tpc.php
@@ -0,0 +1,14 @@
+<?php
+
+include('htmlTPC.php');
+
+$encoding = 'Windows-1251';
+$src_file = 'Contents.htm';
+
+$app = new htmlTPC($encoding);
+$app->load($src_file);
+$app->analyze(true);
+$app->convert('UTF-8');
+$app->save();
+
+?>
